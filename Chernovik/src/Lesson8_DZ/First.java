1960a828ddaad25774136325f4ac4e1163e0e0a6
mozyakov <s.mozyakov@gmail.com>
2022-08-04 21:41:38 +0700

    косметические правки по ДЗ 8 лекции

--- Chernovik/src/Lesson8_DZ/First.java
+++ Chernovik/src/Lesson8_DZ/First.java
@@ -2,26 +2,20 @@ package Lesson8_DZ;
 
 public class First {
     static int tripleMultiply(int a, int b, int c) {
-        int result = a*b*c;
-        return result;
+        return a*b*c;
     }
 
     static void divide(int a, int b) {
-        System.out.println("Частное от деления " +a+ " на " +b+ " = " + a/b);
-        System.out.println("Остаток от деления " +a+ " на " +b+ " = " + a%b);
+        System.out.println("Целое частное от деления "+a+" / "+b+" = "+ a/b +"; Остаток "+ a%b);
     }
 }
 
 class Second {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 3;
-        int c = 7;
-        System.out.println("Применяем метод тройного умножения: "+a+" * "+b+" * "+c+" = " + First.tripleMultiply(a, b, c));
-        System.out.println("Применяем метод тройного умножения: 5 * 2 * 3 = " + First.tripleMultiply(5, 2, 3));
+        System.out.println("Применяем метод тройного умножения:  = " + First.tripleMultiply(10, 3, 7));
+        System.out.println("Применяем метод тройного умножения:  = " + First.tripleMultiply(5, 2, 3));
         System.out.println();
         First.divide(10, 3);
-        System.out.println();
         First.divide(20, 6);
 
 
--- Chernovik/src/Lesson8_DZ/Test1.java
+++ Chernovik/src/Lesson8_DZ/Test1.java
@@ -7,20 +7,22 @@ public class Test1 {
         this.name = name;
         this.rad = rad;
     }
-    static final double p = 3.14;
+    static final double PI = 3.14;
 
     double areaCurcle(double rad) { //метод возвращающий площадь круга
-        double area = p * rad * rad;
+        double area = PI * rad * rad;
         return area;
     }
 
     static double dlinaCurcle(double rad) { //метод возвращающий длину окружности
-        double dlina = 2 * p * rad;
+        double dlina = 2 * PI * rad;
         return dlina;
     }
 
-    void showInfo(double rad) { //выводит инфу о радиусе, площади круга и длине окружности
-        System.out.println("Если у круга радиус="+rad + ", то площадь круга=" + areaCurcle(rad) + ", длина окружности=" + dlinaCurcle(rad));
+    void info(double rad) { //выводит инфу о радиусе, площади круга и длине окружности
+        System.out.println("Радиус круга = " +rad);
+        System.out.println("Площадь круга = " + areaCurcle(rad));
+        System.out.println("Длина окружности = " + dlinaCurcle(rad));
     }
 }
 
@@ -28,8 +30,10 @@ class Test2 {
     public static void main(String[] args) {
         Test1 c1 = new Test1("Krug", 8);
         System.out.println("Площадь круга "+c1.name+" с радиусом "+c1.rad+" равна "+c1.areaCurcle(c1.rad));
-        System.out.println("Длина окружности круга "+c1.name+" с радиусом "+c1.rad+" равна "+c1.dlinaCurcle(c1.rad));
-        c1.showInfo(c1.rad);
+        System.out.println("===============================");
+        System.out.println("Длина окружности круга "+c1.name+" с радиусом "+c1.rad+" равна "+Test1.dlinaCurcle(c1.rad));
+        System.out.println("===============================");
+        c1.info(c1.rad);
 
 
     }
