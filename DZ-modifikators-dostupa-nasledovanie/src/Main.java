52d89189b32e4fc23d51cdc6be2660cdaf3da1c4
mozyakov <s.mozyakov@gmail.com>
2022-08-06 15:36:55 +0700

    Update Main.java

--- DZ-modifikators-dostupa-nasledovanie/src/Main.java
+++ DZ-modifikators-dostupa-nasledovanie/src/Main.java
@@ -7,16 +7,16 @@ public class Main {
     public static void main(String[] args) {
         /*
         SavingsAccount saveAcc = new SavingsAccount("накопительный счет", 10_000, 1_000);
-        saveAcc.pay(5_000); //снятие
-        saveAcc.pay(5_000); //false снятие не пройдет - нельзя опускать ниже мин.баланса
-        saveAcc.add(10_000);   //пополнение будет true
+        saveAcc.pay(5_000); //снятие успешно
+        saveAcc.pay(5_000); //снятие не пройдет, нельзя опускать ниже мин.баланса
+        saveAcc.add(10_000);   //пополнение успешно
         */
 
         /*
         CheckingAccount checkAcc = new CheckingAccount("расчетный счет", 10_000);
 
         checkAcc.pay(10_000); //снятие успешно
-        checkAcc.pay(10_000); //снятие не пройдет, в минус уходить нельзя
+        checkAcc.pay(10_000); //снятие не пройдет, нельзя уходить в минус
         checkAcc.add(20_000);   //пополнение успешно
          */
 
