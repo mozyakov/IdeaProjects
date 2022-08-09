0fe1e5d2bf6e826943ef0568fbec884c61d8fe1f
mozyakov <s.mozyakov@gmail.com>
2022-08-09 17:27:20 +0700

    Лекция-11. сделано задание.

--- Chernovik/src/HomeWork/Lesson11/Car.java
+++ Chernovik/src/HomeWork/Lesson11/Car.java
@@ -1,22 +1,28 @@
 package HomeWork.Lesson11;
 public class Car {
-    String color; String engine; int countDoors;
-    Car() {};
-
-    Car(String color, String engine, int countDoors) {
-        this.color = color; this.engine = engine; this.countDoors = countDoors;
+    String color;
+    String engine;
+    int countDoors;
+    public Car(String color, String engine, int countDoors) {
+        this.color = color;
+        this.engine = engine;
+        this.countDoors = countDoors;
     }
+    /*public void changeDoors(int countDoors) { //но задание сделать этот метод внутри CarTest
+      this.countDoors = countDoors;
+    } */
 }
 class CarTest{
-    public static void changeDoors(Car car) { //меняю кол-во дверей
-        car.countDoors += 2;
+    //public static void changeDoors(Car car) { //меняю кол-во дверей но условия задачи другие
+        //car.countDoors += 2;
+    //}
+    static void changeCountDoors(Car c, int countDoors) {
+        c.countDoors = countDoors;
     }
-    public static void changeColor(Car car1, Car car2) { //меняю цвет у объектов
-        //car3 = car1.color;
-        Car car3 = new Car();
-        car3.color = car1.color;
+    static void changeColor(Car car1, Car car2) { //меняю цвет у объектов
+        String color = car1.color;
         car1.color = car2.color;
-        car2.color = car3.color;
+        car2.color = color;
     }
     public static void main(String[] args) {
         Car lada = new Car("white", "V4", 2);
@@ -26,8 +32,9 @@ class CarTest{
         System.out.println("volga color : " + volga.color);
         System.out.println("volga countDoors : "  + volga.countDoors);
         System.out.println("=====ниже изменения=====");
-        changeDoors(lada);
-        changeColor(lada, volga);
+
+        changeCountDoors(lada, 4); //меняю кол-во дверей
+        changeColor(lada, volga);  //меняю цвета машин между собой
         System.out.println("lada color : "  + lada.color);
         System.out.println("lada countDoors : "  + lada.countDoors);
         System.out.println("volga color : "  + volga.color);
