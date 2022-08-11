614da626273fbb75843127c3c341a2c2c5d80f17
mozyakov <s.mozyakov@gmail.com>
2022-08-11 17:32:39 +0700

    HomeWork.Lesson14.ok

--- Chernovik/src/HomeWork/Lesson14/Time.java
+++ Chernovik/src/HomeWork/Lesson14/Time.java
@@ -1,24 +1,26 @@
 package HomeWork.Lesson14;
 
 public class Time {
-    public void time() {   //показ времени в формате чч:мм
-        for(int hour = 0; hour <=23; hour++) {
-            for(int min = 00; min <= 59; min++) {
-                if(hour > 1 && min%10 == 0) {
+
+    static void time() {   //показ времени в формате чч:мм
+        for (int hour = 0; hour < 6; hour++) {
+            for (int min = 0; min < 60; min++) {
+                if (hour > 1 && min % 10 == 0) {
                     break;
                 }
-                for(int sec = 00; sec <= 59; sec++)
-                    if(hour < 6) {
-                        System.out.println(hour + ":" + min + ":" + sec);
+                for (int sec = 0; sec < 60; sec++) {
+                    if (sec*hour > min) {
+                        continue;
                     }
+                    System.out.println(hour + ":" + min + ":" + sec);
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        Time t = new Time();
-        t.time();
+        time();
+        System.out.println(30 % 10); //пример числа кратного десяти, должно быть 0 в остатке
+
     }
-
-
 }
