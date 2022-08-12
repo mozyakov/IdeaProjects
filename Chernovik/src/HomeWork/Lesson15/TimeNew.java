b7b28bfa0d13020a046a3e56092532ab6bb4421b
mozyakov <s.mozyakov@gmail.com>
2022-08-12 12:18:15 +0700

    Лекция15, ДЗ-ок

---
+++ Chernovik/src/HomeWork/Lesson15/TimeNew.java
@@ -0,0 +1,40 @@
+package HomeWork.Lesson15;
+public class TimeNew {
+    public static void time() {   //показ времени в формате чч:мм:cc
+        int hour = 0;
+
+        OUTER:
+        while (hour < 6) {
+            int min = -1;
+
+            MIDDLE:
+            do {
+                min++;
+                if (hour > 1 && min % 10 == 0) { //часть про минуты не срабатывает!
+                    break OUTER;
+                }
+
+                int sec = 0;
+                INNER:
+                while (sec < 60) {
+                    if (sec*hour > min) {
+                        continue MIDDLE;
+                    }
+                    System.out.println(hour + ":" + min + ":" + sec);
+                    sec++;
+
+                }
+            } while (min < 59);
+            hour++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        time();
+        //System.out.println(10 % 10); //пример числа кратного десяти, должно быть 0 в остатке
+        //значит в цикле должно было остановить на 1 часу 10 минуте, а он продолжил до 2 часов
+
+
+    }
+}
--- Chernovik/src/Lesson15_while_do_while/Test2.java
+++ Chernovik/src/Lesson15_while_do_while/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         int money = 100;
         while(money > 0) {
+            System.out.println("На вашем счету : " + money);
             System.out.println("Делайте ставку");
             System.out.println("Вы проиграли");
             money -= 10;
