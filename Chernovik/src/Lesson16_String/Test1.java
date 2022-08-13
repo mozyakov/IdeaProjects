ab5b4cec58b8ee8cd6f10050513a5526f124f0f5
mozyakov <s.mozyakov@gmail.com>
2022-08-13 11:12:01 +0700

    lection16 contonue

--- Chernovik/src/Lesson16_String/Test1.java
+++ Chernovik/src/Lesson16_String/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
     public static void main(String[] args) {
 
         String s1 = new String("Privet");
+        String s2 = new String("abcdefabc");
         int a = s1.length();
         System.out.println(a);
         System.out.println(s1.length()); //кол-во символов
@@ -21,6 +22,11 @@ public class Test1 {
         int i2 = s1.indexOf("vet");
         System.out.println(i2);
         System.out.println("Papa".indexOf("z"));  //вызов несуществ.индекса покажет -1
+
         System.out.println("abcdefgabcd".indexOf("a", 5)); //показ индекс а начиная с пятой позиции
+
+        System.out.println(s1.startsWith("abc"));  //false птмчт s1 НЕ начинается с abc
+        System.out.println(s2.startsWith("abc"));  //true
+
     }
 }
