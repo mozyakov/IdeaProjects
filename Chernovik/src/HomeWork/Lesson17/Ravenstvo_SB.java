a99630ffec5a7b2753838cdbab9f883d05e116d4
mozyakov <s.mozyakov@gmail.com>
2022-08-16 20:58:34 +0700

    1

--- Chernovik/src/HomeWork/Lesson17/Ravenstvo_SB.java
+++ Chernovik/src/HomeWork/Lesson17/Ravenstvo_SB.java
@@ -1,15 +1,13 @@
 package HomeWork.Lesson17;
-
 public class Ravenstvo_SB {
-
-        public static boolean ravenstvo(StringBuilder y1, StringBuilder y2) {
-            if(y1.length() == y2.length()) {
-                for(int i = 0; i <y1.length(); i++) {
-                    if(y1.charAt(i) != y2.charAt(i)) {
-                        return false;
+        public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
+            if(sb1.length() == sb2.length()) {    //длина должна совпадать
+                for(int i = 0; i <sb1.length(); i++) {
+                    if(sb1.charAt(i) != sb2.charAt(i)) {   //сравнение посимвольно
+                        return false;     //если символы разные сразу конец
                     }
                 }
-            }else {
+            }else {   //если длина разная то конец
                 return false;
             }
             return true;
@@ -19,10 +17,10 @@ public class Ravenstvo_SB {
         StringBuilder sb1 = new StringBuilder("123");
         StringBuilder sb2 = new StringBuilder("123");
         StringBuilder sb3 = new StringBuilder("hello");
-        StringBuilder sb4 = new StringBuilder("hello");
+        StringBuilder sb4 = new StringBuilder("helloooo");
         StringBuilder sb5 = new StringBuilder("Hello");
 
-        System.out.println(ravenstvo(sb3, sb4));
-        System.out.println(Ravenstvo_SB.ravenstvo(new StringBuilder(""), new StringBuilder("")));
+        //System.out.println(Ravenstvo_SB.ravenstvo(new StringBuilder(""), new StringBuilder("")));
+        System.out.println(ravenstvo(sb3, sb5));
     }
 }
