01a71bfe60a9ec76e464e3114f5b9f0354171d53
mozyakov <s.mozyakov@gmail.com>
2022-08-19 01:06:11 +0700

    dz-1 нашел решение

---
+++ Chernovik/src/HomeWork/Lesson18/Task2.java
@@ -0,0 +1,11 @@
+package HomeWork.Lesson18;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String[][] str = {{"PapaMamaDetiSobaki"}, {"MamaPapaDetiSobaki"}};
+    }
+    public void String(String[][] str) {
+
+
+    }
+}
--- Chernovik/src/Lesson18_Arrays/Test2_dynamic_initialization.java
+++ Chernovik/src/Lesson18_Arrays/Test2_dynamic_initialization.java
@@ -9,10 +9,10 @@ public class Test2_dynamic_initialization {
         arr2 = new int[3][]; //пустое [] разное кол-во
 
         //dynamic initialization это когда значения присваиваются не вручную, а автоматически
-        for (int i = 0; i < arr1.length; i++) {
+        /*for (int i = 0; i < arr1.length; i++) {
             arr1[i] = "privet" + i;
             System.out.println(arr1[i]);
-        }
+        }*/
 
         arr2[0] = new int[5];
         arr2[1] = new int[2];
