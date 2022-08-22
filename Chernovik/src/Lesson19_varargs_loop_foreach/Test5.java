99b7666f438a2cc3c276b9d4810eb2eea1a1e079
mozyakov <s.mozyakov@gmail.com>
2022-08-22 20:32:19 +0700

    +1 loop for each

--- Chernovik/src/Lesson19_varargs_loop_foreach/Test5.java
+++ Chernovik/src/Lesson19_varargs_loop_foreach/Test5.java
@@ -3,6 +3,14 @@ package Lesson19_varargs_loop_foreach;
 public class Test5 {
     public static void main(String[] args) {
         String[] students = {"Ivanov", "Petrov", "Sidorov"};
-        String[] examz = {"Mat.analiz", "filosofia"};
+        String[] examz = {"mat.analiz", "filosofia"};
+        for(String s1: students) {
+            for(String s2: examz) {
+                System.out.println(s1 + " " + s2);
+            }
+        }
+
+
+
     }
 }
