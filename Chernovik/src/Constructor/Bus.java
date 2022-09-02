98318893c071ca0343e036e8791bc8b35ed46c34
mozyakov <s.mozyakov@gmail.com>
2022-09-02 20:36:39 +0700

    1

--- Chernovik/src/Constructor/Bus.java
+++ Chernovik/src/Constructor/Bus.java
@@ -3,13 +3,10 @@ package Constructor;
 public class Bus {
     private String name;
     private String color = "red"; //параметр по умолчанию
-
     //Bus(String name) { //если создаем конструктор с параметрами, то дефолтный отменяется и
        //this.name = name;
    // }
     //Bus() {}; //надо дефолтный конструктор выписать отдельно, если нужно создавать без параметров
-
-
     public void setName(String s) {
         name = s;
     }
