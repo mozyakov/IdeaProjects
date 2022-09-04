f52beec0e340742e842bff46358f65b2cd3a4f21
mozyakov <s.mozyakov@gmail.com>
2022-09-04 21:25:43 +0700

    Update.Bus

--- Chernovik/src/Constructor/Bus.java
+++ Chernovik/src/Constructor/Bus.java
@@ -21,9 +21,18 @@ public class Bus {
 class Test {
     public static void main(String[] args) {
         Bus bus1 = new Bus();
+        Bus bus2 = new Bus();
+        Bus bus3 = new Bus();
         bus1.setName("ford");
+        bus2.setName("man");
+        bus3.setName("liaz");
         System.out.println(bus1.getName());
         System.out.println(bus1.getColor());
+        System.out.println("================");
+        Bus[] arr = {bus1, bus2, bus3};
+        for(Bus b: arr) {
+            System.out.println(b.getName());
+        }
     }
 }
 
